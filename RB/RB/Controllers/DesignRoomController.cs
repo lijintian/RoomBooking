e909eeecebcc995e0e6abbedb0674be21ae9fc8e
jintian.li <jintian.li@chukou1.com>
2016-07-07 18:07:26 +0800

    part-add（10%）:room design tool

---
+++ RB/RB/Controllers/DesignRoomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RB.Controllers
+{
+    public class DesignRoomController : Controller
+    {
+        // GET: DesignRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RB/RB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RB.Startup))]
+namespace RB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
