087307fb101b6854d77c5f9fd0f24df66806fadb
jintian.li <jintian.li@chukou1.com>
2016-07-12 15:53:42 +0800

    add:添加属性栏，选中State中的对象时会显示熟悉栏

---
+++ RB/RB/Scripts/DesignRoom/PropertyBar.js
@@ -0,0 +1,11 @@
+﻿/*
+* 属性栏
+@method PropertyBar
+@param {Position} position 位置信息
+@param {Size} size 尺寸信息
+@return {PropertyBar} 属性栏对象
+*/
+function PropertyBar(position, size)
+{
+    BaseObject.apply(this, new Array(position, size, PropertyBar.name));
+}
