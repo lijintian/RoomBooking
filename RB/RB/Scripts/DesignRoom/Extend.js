b1e3516da7097f03bc8fad4767a653c43138a69f
jintian.li <jintian.li@chukou1.com>
2016-07-18 11:18:32 +0800

    add:多选操作添加平均水平，垂直间距功能

--- RB/RB/Scripts/DesignRoom/Extend.js
+++ RB/RB/Scripts/DesignRoom/Extend.js
@@ -6,7 +6,18 @@ Array.prototype.alignLeft = alignLeft;
 Array.prototype.alignRight = alignRight;
 Array.prototype.isSameSize = isSameSize;
 Array.prototype.isSameName = isSameName;
+Array.prototype.findMostTop = findMostTop;
+Array.prototype.findMostButtom = findMostButtom;
+Array.prototype.findMostLeft = findMostLeft;
+Array.prototype.findMostRight = findMostRight;
+Array.prototype.sortByPositionX = sortByPositionX;
+Array.prototype.sortByPositionY = sortByPositionY;
+Array.prototype.horizonAverage = horizonAverage;
+Array.prototype.verticalAverage = verticalAverage;
 
+/*
+@method 判断亮哥数组是否相同(position,size相同及认为相同)
+*/
 function arrayEqual(arr) {
     if (this.length == arr.length) {
         for (var i = 0; i < arr.length; i++)
@@ -23,6 +34,9 @@ function arrayEqual(arr) {
     }
 }
 
+/*
+@method 克隆数组
+*/
 function arrayClone() {
     var arr = new Array();
 
@@ -33,21 +47,13 @@ function arrayClone() {
     return arr;
 }
 
+/*
+@method 数组元素上对齐
+*/
 function alignTop() {
     var returnNum = 0;
 
-    if (this.length > 0)
-    {
-        returnNum = this[0].position.y;
-
-        for (var i = 1; i < this.length ; i++) {
-            var thisY=this[i].position.y;
-            if(thisY<returnNum)
-            {
-                returnNum = thisY;
-            }
-        }
-    }
+    returnNum = this.findMostTop();
 
     for (var i = 0; i < this.length ; i++) {
         this[i].position.y = returnNum;
@@ -56,19 +62,13 @@ function alignTop() {
     return returnNum;
 }
 
+/*
+@method 数组元素下对齐
+*/
 function alignButtom() {
     var returnNum = 0;
 
-    if (this.length > 0) {
-        returnNum = this[0].position.y;
-
-        for (var i  = 1; i < this.length ; i++) {
-            var thisY = this[i].position.y ;
-            if (thisY > returnNum) {
-                returnNum = thisY;
-            }
-        }
-    }
+    returnNum = this.findMostButtom();
 
     for (var i = 0; i < this.length ; i++) {
         this[i].position.y = returnNum;
@@ -76,19 +76,13 @@ function alignButtom() {
     return returnNum;
 }
 
+/*
+@method 数组元素左对齐
+*/
 function alignLeft() {
     var returnNum = 0;
 
-    if (this.length > 0) {
-        returnNum = this[0].position.x;
-
-        for (var i = 1; i < this.length ; i++) {
-            var thisX = this[i].position.x;
-            if (thisX < returnNum) {
-                returnNum = thisX;
-            }
-        }
-    }
+    returnNum = this.findMostLeft();
 
     for (var i = 0; i < this.length ; i++) {
         this[i].position.x = returnNum;
@@ -97,19 +91,14 @@ function alignLeft() {
     return returnNum;
 }
 
+/*
+@method 数组元素右对齐
+*/
 function alignRight() {
     var returnNum = 0;
 
-    if (this.length > 0) {
-        returnNum = this[0].position.x;
-
-        for (var i  = 1; i < this.length ; i++) {
-            var thisX = this[i].position.x;
-            if (thisX > returnNum) {
-                returnNum = thisX;
-            }
-        }
-    }
+    returnNum = this.findMostRight();
+  
     for (var i = 0; i < this.length ; i++) {
         this[i].position.x = returnNum;
     }
@@ -117,6 +106,9 @@ function alignRight() {
     return returnNum;
 }
 
+/*
+@method 判断数组元素的尺寸是否都一样
+*/
 function isSameSize() {
     var isSame = true;
 
@@ -136,6 +128,9 @@ function isSameSize() {
     return isSame;
 }
 
+/*
+@method 判断数组元素的Name是否都一样
+*/
 function isSameName() {
     var isSame = true;
 
@@ -152,5 +147,158 @@ function isSameName() {
         }
     }
     return isSame;
+}
+
+/*
+@method 找数组元素的最上位置 
+*/
+function findMostTop()
+{
+    var returnNum = 0;
+
+    if (this.length > 0) {
+        returnNum = this[0].position.y;
+
+        for (var i = 1; i < this.length ; i++) {
+            var thisY = this[i].position.y;
+            if (thisY < returnNum) {
+                returnNum = thisY;
+            }
+        }
+    }
+
+    return returnNum;
+}
+
+/*
+@method 找数组元素的最下位置
+*/
+function findMostButtom() {
+    var returnNum = 0;
+
+    if (this.length > 0) {
+        returnNum = this[0].position.y;
+
+        for (var i = 1; i < this.length ; i++) {
+            var thisY = this[i].position.y;
+            if (thisY > returnNum) {
+                returnNum = thisY;
+            }
+        }
+    }
+
+    return returnNum;
+}
+
+/*
+@method 找数组元素的最左位置
+*/
+function findMostLeft() {
+    var returnNum = 0;
+
+    if (this.length > 0) {
+        returnNum = this[0].position.x;
+
+        for (var i = 1; i < this.length ; i++) {
+            var thisX = this[i].position.x;
+            if (thisX < returnNum) {
+                returnNum = thisX;
+            }
+        }
+    }
+
+    return returnNum;
+}
+
+/*
+@method 找数组元素的最右位置
+*/
+function findMostRight() {
+    var returnNum = 0;
+
+    if (this.length > 0) {
+        returnNum = this[0].position.x;
+
+        for (var i = 1; i < this.length ; i++) {
+            var thisX = this[i].position.x;
+            if (thisX > returnNum) {
+                returnNum = thisX;
+            }
+        }
+    }
+
+    return returnNum;
+}
+
+/*
+@method 根据position.x进行排序
+*/
+function sortByPositionX()
+{
+    //冒泡
+    for(var i=0;i<this.length;i++)
+    {
+        for(var j=i;j<this.length;j++)
+        {
+            if(this[i].position.x>this[j].position.x)
+            {
+                var temp=this[j];
+                this[j]=this[i];
+                this[i]=temp;
+            }
+        }
+    }
+}
+
+/*
+@method 根据position.y进行排序
+*/
+function sortByPositionY() {
+    //冒泡
+    for (var i = 0; i < this.length; i++) {
+        for (var j = i; j < this.length; j++) {
+            if (this[i].position.y > this[j].position.y) {
+                var temp = this[j];
+                this[j] = this[i];
+                this[i] = temp;
+            }
+        }
+    }
+}
+
+
+//横向平均
+function horizonAverage()
+{
+    var leftX = this.findMostLeft();
+    var rightX = this.findMostRight();
+    var elementCount=this.length;
+    var averageDistance=(rightX-leftX)/(elementCount-1)
+
+    this.sortByPositionX()
+
+    var nextX = leftX;
+    for (var i = 0; i < this.length; i++)
+    {
+        this[i].position.x = nextX;
+        nextX += averageDistance;
+    }
+}
+
+//纵向平均
+function verticalAverage()
+{
+    var topY = this.findMostTop();
+    var buttomY = this.findMostButtom();
+    var elementCount = this.length;
+    var averageDistance = (buttomY - topY) / (elementCount - 1)
+
+    this.sortByPositionY()
+
+    var nextY = topY;
+    for (var i = 0; i < this.length; i++) {
+        this[i].position.y = nextY;
+        nextY += averageDistance;
+    }
 }
 
